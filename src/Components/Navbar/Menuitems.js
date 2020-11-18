9a4199dea255ecc4c17d454071c9e68a952e56a9
surakshns <surakshnss@gmail.com>
2020-11-18 19:17:41 +0530

    event page

--- src/Components/Navbar/Menuitems.js
+++ src/Components/Navbar/Menuitems.js
@@ -4,27 +4,27 @@ const Menuitem = [
     {
         cname: 'nav-items',
         name: 'Home',
-        link: '/ISSA_Dynamic_Web/',
+        link: 'ISSA-dynamic/',
     },
     {
         cname: 'nav-items',
         name: 'Events',
-        link: '/ISSA_Dynamic_Web//',
+        link: 'ISSA-dynamic//',
     },
     {
         cname: 'nav-items',
         name: 'Projects',
-        link: '/ISSA_Dynamic_Web///',
+        link: 'ISSA-dynamic///',
     },
     {
         cname: 'nav-items',
         name: 'Your-Blog',
-        link: '/ISSA_Dynamic_Web////',
+        link: 'ISSA-dynamic////',
     },
     {
         cname: 'nav-items',
         name: 'Register',
-        link: '/ISSA_Dynamic_Web/////',
+        link: 'ISSA-dynamic/////',
     }
 ];
 export default Menuitem;
