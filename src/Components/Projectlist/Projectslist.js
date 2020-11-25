e14c59f3f53dcc9a44206bc29193b53f1c2604b8
Pooja P <poojaprabhu567@gmail.com>
2020-11-25 19:02:27 +0530

    Added Projectlist js files

---
+++ src/Components/Projectlist/Projectslist.js
@@ -0,0 +1,101 @@
+import React, { Component } from 'react';
+import Card from './Card';
+
+
+class Projectslist extends Component{
+    
+    render(){
+        return(
+            <div >
+
+
+            <div className='App' >
+           
+           <Card 
+           title='Project Title'
+           imageUrl='https://picsum.photos/seed/picsum/200/200'
+           body='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vel ullamcorper quam. Cras ultricies aliquam sem, vel sollicitudin nunc imperdiet dictum. Morbi lacinia augue et maximus volutpat. Proin euismod laoreet ante non tempor. Cras augue ex, bibendum ac lectus eu, pretium mollis'
+           
+           />
+           </div>
+
+
+           <div className='App' >
+           
+           <Card 
+           title='Project Title'
+           imageUrl='https://picsum.photos/seed/picsum/200/200'
+           body='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vel ullamcorper quam. Cras ultricies aliquam sem, vel sollicitudin nunc imperdiet dictum. Morbi lacinia augue et maximus volutpat. Proin euismod laoreet ante non tempor. Cras augue ex, bibendum ac lectus eu, pretium mollis'
+           
+           />
+           </div>
+           
+           <div className='App'>
+           
+           <Card 
+           title='Project Title'
+           imageUrl='https://picsum.photos/seed/picsum/200/200'
+           body='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vel ullamcorper quam. Cras ultricies aliquam sem, vel sollicitudin nunc imperdiet dictum. Morbi lacinia augue et maximus volutpat. Proin euismod laoreet ante non tempor. Cras augue ex, bibendum ac lectus eu, pretium mollis'
+           
+           />
+           </div>
+
+           <div className='App'>
+           
+           <Card 
+           title='Project Title'
+           imageUrl='https://picsum.photos/seed/picsum/200/200'
+           body='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vel ullamcorper quam. Cras ultricies aliquam sem, vel sollicitudin nunc imperdiet dictum. Morbi lacinia augue et maximus volutpat. Proin euismod laoreet ante non tempor. Cras augue ex, bibendum ac lectus eu, pretium mollis'
+           
+           />
+           </div>
+
+
+           <div className='App'>
+           
+           <Card 
+           title='Project Title'
+           imageUrl='https://picsum.photos/seed/picsum/200/200'
+           body='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vel ullamcorper quam. Cras ultricies aliquam sem, vel sollicitudin nunc imperdiet dictum. Morbi lacinia augue et maximus volutpat. Proin euismod laoreet ante non tempor. Cras augue ex, bibendum ac lectus eu, pretium mollis'
+           
+           />
+           </div>
+
+
+           <div className='App'>
+           
+           <Card 
+           title='Project Title'
+           imageUrl='https://picsum.photos/seed/picsum/200/200'
+           body='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vel ullamcorper quam. Cras ultricies aliquam sem, vel sollicitudin nunc imperdiet dictum. Morbi lacinia augue et maximus volutpat. Proin euismod laoreet ante non tempor. Cras augue ex, bibendum ac lectus eu, pretium mollis'
+           
+           />
+           </div>
+
+           <div className='App'>
+           
+           <Card 
+           title='Project Title'
+           imageUrl='https://picsum.photos/seed/picsum/200/200'
+           body='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vel ullamcorper quam. Cras ultricies aliquam sem, vel sollicitudin nunc imperdiet dictum. Morbi lacinia augue et maximus volutpat. Proin euismod laoreet ante non tempor. Cras augue ex, bibendum ac lectus eu, pretium mollis'
+           
+           />
+           </div>
+
+           <div className='App'>
+           
+           <Card 
+           title='Project Title'
+           imageUrl='https://picsum.photos/seed/picsum/200/200'
+           body='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vel ullamcorper quam. Cras ultricies aliquam sem, vel sollicitudin nunc imperdiet dictum. Morbi lacinia augue et maximus volutpat. Proin euismod laoreet ante non tempor. Cras augue ex, bibendum ac lectus eu, pretium mollis'
+           
+           />
+           </div>
+
+
+           </div>
+        );
+    }
+}
+
+export default Projectslist
