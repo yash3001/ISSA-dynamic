bf73d1c798a68f40b046f31bd812638ac088a603
Pooja P <poojaprabhu567@gmail.com>
2020-12-15 17:38:41 +0530

    Added Background gradient to cardjs

--- src/Components/Projectlist/Card.js
+++ src/Components/Projectlist/Card.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import './Card.css';
 
+
+
 function Card({title,imageUrl,body}){
+    
     return(
+        <div className="application">
+            
+        <style>{'body {  height: 100vh;background-color: #009FFF;background-image: linear-gradient(to bottom right, #009FFF, #ec2F4B);line-height: 200%;border-color:black;border-width:5px;border-style:solid; }'}</style>
+    
+        
         <div className='card-container'>
             <div className='image-container'>
                 <img src={imageUrl} alt=''/>
@@ -20,11 +28,16 @@ function Card({title,imageUrl,body}){
                         </a>
                     </button>
                 </div>
-            </div>
+                
+                </div> 
+                </div>
+             
+                
+        
+        
     )
 
 }
  
-
 
 export default Card;
