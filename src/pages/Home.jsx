c57a9d1fc198466c25cd030075f93328e6f9bfb8
yash3001 <yashchauhan197@gmail.com>
2020-12-23 15:50:02 +0530

    implemented the timer functionality

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useRef, useEffect } from "react";
 import Header from "../Components/Header/Header";
 import About from "../Components/About/About";
 import Timer from "../Components/Timer/Timer";
@@ -6,29 +6,69 @@ import Team from "../Components/Team/Team";
 import Navigation from "../Components/Navigation/Navigation";
 import Toggler from "../Components/Navigation/Toggler";
 
-class Home extends Component{
-    state = {
-        timer: [3, 16, 36],
-      };
-      componentDidMount() {
-        window.scrollTo(0, 0);
-    }
-      render(){
-          return(
-      <div className='homepage'>
-        
-        <main>
-          <Header />
-          <About />
-          <Timer
-            value_days={this.state.timer[0]}
-            value_hours={this.state.timer[1]}
-            value_minutes={this.state.timer[2]}
-          />
-          <Team />
-        </main>
-      </div>
-          );
+const Home = () => {
+  const [timerDays, setTimerDays] = useState("00");
+  const [timerHours, setTimerHours] = useState("00");
+  const [timerMinutes, setTimerMinutes] = useState("00");
+
+  let interval = useRef();
+
+  const startTimer = () => {
+    const countdownDate = new Date("December 26, 2020 17:00:00").getTime();
+
+    interval = setInterval(() => {
+      const now = new Date().getTime();
+      const distance = countdownDate - now;
+
+      let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      let hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+
+      if (distance < 0) {
+        clearInterval(interval.current);
+      } else {
+        if (days < 10) {
+          days = "0" + days;
+        }
+        if (hours < 10) {
+          hours = "0" + hours;
+        }
+        if (minutes < 10) {
+          minutes = "0" + minutes;
+        }
+        setTimerDays(days);
+        setTimerHours(hours);
+        setTimerMinutes(minutes);
       }
-}
+    }, 1000);
+  };
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  useEffect(() => {
+    startTimer();
+    return () => {
+      clearInterval(interval.current);
+    };
+  });
+
+  return (
+    <div className="homepage">
+      <main>
+        <Header />
+        <About />
+        <Timer
+          value_days={timerDays}
+          value_hours={timerHours}
+          value_minutes={timerMinutes}
+        />
+        <Team />
+      </main>
+    </div>
+  );
+};
 export default Home;
