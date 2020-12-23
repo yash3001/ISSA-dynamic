ea61039467aebfb29f1a3f79753d16b14f8e15fd
yash3001 <yashchauhan197@gmail.com>
2020-12-23 15:49:27 +0530

    removed the unnecessary statement

--- src/Components/Eventslist/Cards.js
+++ src/Components/Eventslist/Cards.js
@@ -1,35 +1,49 @@
-import React,{Component} from 'react';
-import './Elists.css'
+import React, { Component } from "react";
+import "./Elists.css";
 
-function Cards( props ) {
-    const switchs = props.switchs;
-    const images = props.image;
-    const youtube = props.youtube;
-    function Change(props){
-        console.log(youtube);
-    if (switchs && youtube!=undefined && youtube!=''){
-        return <iframe className='card-img event-image w-100'  src={youtube} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+function Cards(props) {
+  const switchs = props.switchs;
+  const images = props.image;
+  const youtube = props.youtube;
+  function Change(props) {
+    console.log(youtube);
+    if (switchs && youtube != undefined && youtube != "") {
+      return (
+        <iframe
+          className="card-img event-image w-100"
+          src={youtube}
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+      );
+    } else {
+      return (
+        <img
+          class="card-img event-image w-100"
+          src={images}
+          alt="Card image cap"
+        />
+      );
     }
-    else{
-        return <img class="card-img event-image w-100" src={images} alt="Card image cap" /> 
-    }
-    }
-    return(
-        <div>
-    <li>
-            <div class="card elistcard">
-                <Change />
-                <div class="card-body">
-                    <h5 class="card-title ename">{props.name}</h5>
-                    <p class="card-text ediscription">{props.description}</p>
-                    <div>{props.children}</div>
-                    <div class="card-text d-flex justify-content-center edate">{props.date}</div>
-                </div>
+  }
+  return (
+    <div>
+      <li>
+        <div class="card elistcard">
+          <Change />
+          <div class="card-body">
+            <h5 class="card-title ename">{props.name}</h5>
+            <p class="card-text ediscription">{props.description}</p>
+            <div>{props.children}</div>
+            <div class="card-text d-flex justify-content-center edate">
+              {props.date}
             </div>
-    </li>
-    <br />
+          </div>
+        </div>
+      </li>
+      <br />
     </div>
-    )
-    switchs != props.switchs;
-};
+  );
+}
 export default Cards;
